10bb56d44ed08c87932871aaa704dd21be069bd8
secarrel <secarrel@outlook.com>
2024-05-07 09:34:23 +0100

    Update: nesting components

--- my-app/src/components/NestingComponents.js
+++ my-app/src/components/NestingComponents.js
@@ -1,31 +1,20 @@
 import React, { Component } from 'react'
+import UserMessage from './UserMessage'
+import UserData from './UserData'
 
 class NestingComponents extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoaded: false,
-            isLoggedIn: true,
+            isLoaded: true,
+            isLoggedIn: false,
         }
     }
     render() {
         return (
             <div>
-                <h1>{this.state.isLoaded ? 'Data loaded!' : 'Loading ...'}</h1>
-                {
-                    this.state.isLoggedIn ? (
-                        <div>
-                            <p>Welcome to the site! Please complete the steps below:</p>
-                            <ol>
-                                <li>Confirm your email</li>
-                                <li>Complete your profile</li>
-                                <li>Subscribe to the newsletter</li>
-                            </ol>
-                        </div>
-                    ) : (
-                        <p>Please sign in</p>
-                    )
-                }
+                <UserData isLoaded={this.state.isLoaded}/>
+                <UserMessage isLoggedIn={this.state.isLoggedIn}/>
             </div>
         )
     }
